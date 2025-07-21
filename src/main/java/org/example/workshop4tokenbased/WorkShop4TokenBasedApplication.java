349b0537f50061ce51767f4154506cb0cbdb2245
1Lence <kirillpetrov173@gmail.com>
2025-07-21 14:47:30 +0500

    Create-Proj

---
+++ src/main/java/org/example/workshop4tokenbased/WorkShop4TokenBasedApplication.java
@@ -0,0 +1,13 @@
+package org.example.workshop4tokenbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShop4TokenBasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShop4TokenBasedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/workshop4tokenbased/WorkShop4TokenBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workshop4tokenbased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShop4TokenBasedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
